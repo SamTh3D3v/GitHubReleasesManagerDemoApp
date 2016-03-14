02767872706a9943b1b6c921d9eff47483e7ea5c
Abdelkhalek Oussama Elhamer <oussama.elhamer@outlook.com>
2016-03-14 09:08:11 +0100

    Update solution name

--- GitHubReleasesManagerDemeApp/Properties/Settings.Designer.cs
+++ GitHubReleasesManagerDemeApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GitHubReleasesManagerDemeApp.Properties
+namespace GitHubReleasesManagerDemoApp.Properties
 {
 
 
